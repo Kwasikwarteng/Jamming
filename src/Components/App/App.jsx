feccbc6ba12a7d91fb2b4f4323acc6aa8d0a99bb
kwasikwarteng <megakwasikwarteng@gmail.com>
2023-01-04 14:20:45 +0000

    First commit of jamming

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,44 @@
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        {/* <SearchBar /> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>
+    // <div className="App">
+    //    <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
+  );
+}
+
+export default App;
